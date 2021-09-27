0fa949f1c9abbf3ab0525a921148d45fd2a52cf6
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2021-09-27 21:35:13 +0700

    initial commit

---
+++ endpoint.js
@@ -0,0 +1,20 @@
+class Endpoint {
+    static instance = null;
+    static getInstance() {
+        if (Endpoint.instance == null) {
+            Endpoint.instance = new Endpoint();
+        }
+        return Endpoint.instance;
+    }
+
+
+    LOGIN = "http://mhsmobile.amikom.ac.id/login"
+    JADWAL = "http://mhsmobile.amikom.ac.id/api/personal/jadwal_kuliah"
+    PERSONAL = "http://mhsmobile.amikom.ac.id/api/personal/init_data_mhs"
+    LIST_PRESENSI = "http://mhsmobile.amikom.ac.id/api/presensi/list_mk"
+    PRESENSI = "http://202.91.9.14:6000/api/presensi_mobile/validate_qr_code"
+    KHS = "http://mhsmobile.amikom.ac.id/api/krs/init_khs"
+    TRANSKRIP = "http://mhsmobile.amikom.ac.id/api/krs/transkrip"
+}
+
+module.exports = Endpoint.getInstance()
---
+++ index.js
@@ -0,0 +1,126 @@
+const axios = require('axios')
+const Endpoint = require('./endpoint')
+
+
+module.export = class AmikomOne {
+    constructor() {
+        this.access_token = ""
+        this.npm = ""
+        this.request = axios.create({
+            headers: {
+                "user-agent": "@m!k0mXv=#neMob!le",
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
+    }
+
+    set Authorization(token) {
+        this.request.defaults.headers['Authorization'] = token
+    }
+    set ContentType(content_type) {
+        this.request.defaults.headers['Content-Type'] = content_type
+    }
+
+    async makeParam(parameterAdd) {
+        let params = new URLSearchParams()
+        for (let key in parameterAdd)
+            params.append(key, parameterAdd[key])
+        return params.toString()
+    }
+
+    async get() {
+        const response = await this.request.get(this.url)
+        return response.data
+    }
+
+    async login(npm, passcode) {
+        try {
+            let params = await this.makeParam({
+                username: npm,
+                keyword: passcode
+            })
+            const response = await this.request.post(Endpoint.LOGIN, params)
+            this.npm = npm
+            this.access_token = response.data.access_token
+            this.Authorization = this.access_token
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async presensi(data) {
+        try {
+            let params = await this.makeParam({
+                data
+            })
+            const response = await this.request.post(Endpoint.PRESENSI, params, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async list_presensi(semester, tahun_akademik) {
+        try {
+            let params = await this.makeParam({
+                npm: this.npm,
+                semester,
+                tahun_akademik
+            })
+            const response = await this.request.post(Endpoint.LIST_PRESENSI, params)
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    async jadwal() {
+        try {
+            let params = await this.makeParam({
+                npm: this.npm
+            })
+            const response = await this.request.post(Endpoint.JADWAL, params)
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async personal() {
+        try {
+            const response = await this.request.post(Endpoint.PERSONAL)
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async khs() {
+        try {
+            let params = await this.makeParam({
+                npm: this.npm
+            })
+            const response = await this.request.post(Endpoint.KHS, params)
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async transkrip() {
+        try {
+            let params = await this.makeParam({
+                npm: this.npm
+            })
+            const response = await this.request.post(Endpoint.TRANSKRIP, params)
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
