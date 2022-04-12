ef9e3a867e506eae211fc44d72c5315863aa111d
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-04-13 04:47:26 +0700

    ubah import as ke import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Legacy from './Legacy'
 import OneDevice from './OneDevice'
-import * as MikomSupports from './Foundation/Supports'
-import * as MikomValidation from './Foundation/Validations'
-import * as MikomReader from './Foundation/QrReader'
+import MikomSupports from './Foundation/Supports'
+import MikomValidation from './Foundation/Validations'
+import MikomReader from './Foundation/QrReader'
 
 export { Legacy, MikomSupports, MikomValidation, MikomReader }
 export default OneDevice
