b45b1f56e9bcf9927a3f141135d71b57178e2a11
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-04-14 04:47:49 +0700

    build

--- lib/Foundation/QrReader.d.ts
+++ lib/Foundation/QrReader.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="node" />
-export declare const scanWithApi: (data: Buffer) => Promise<string>;
-export default function (data: Buffer): Promise<string>;
+export declare const scanWithApi: (data: Buffer) => Promise<string>
+export default function (data: Buffer): Promise<string>
 //# sourceMappingURL=QrReader.d.ts.map
+
