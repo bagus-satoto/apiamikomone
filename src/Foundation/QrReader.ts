ae9a026605ef1e47cec13a4e1e8444bf1f81f547
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-04-14 04:47:41 +0700

    qr reader with promise any

--- src/Foundation/QrReader.ts
+++ src/Foundation/QrReader.ts
@@ -1,5 +1,4 @@
 import Cheerio from 'cheerio'
-import fileType from 'file-type'
 import got from 'got/dist/source'
 import Jimp from 'jimp'
 import FormData from 'form-data'
@@ -7,93 +6,11 @@ const QrCode = require('qrcode-reader')
 const { getSync } = require('@andreekeberg/imagedata')
 const jsQR = require('jsqr')
 
-const scanWithJimpQrcode = (data: Buffer): Promise<string | null> =>
-  new Promise((resolve) => {
-    Jimp.read(data, function (err, image) {
-      if (err) {
-        resolve(null)
-      }
-      let qrcode = new QrCode()
-      qrcode.callback = function (err2: any, value: any) {
-        if (err2) {
-          resolve(null)
-        }
-        if (value == undefined || value?.result == null || value?.result == '')
-          return resolve(null)
-        resolve(value.result)
-      }
-      qrcode.decode(image.bitmap)
-    })
-  })
-
-export const scanWithApi = async (data: Buffer): Promise<string> => {
-  const filetype = await fileType.fromBuffer(data)
-  if (!filetype) throw new Error('File tidak dikenali')
-
-  const responseRecognize: {
-    success: boolean
-    recognizeResultToken: string
-  } = await got
-    .post(
-      'https://api.products.aspose.app/barcode/recognize/apiRequestRecognize',
-      {
-        form: {
-          type: 'qr',
-          quality: 2,
-          fileBase64: `data:${filetype.mime};base64,${Buffer.from(
-            data
-          ).toString('base64')}`
-        }
-      }
-    )
-    .json()
-  const getReady = async (
-    recognizeResultToken: string
-  ): Promise<{
-    ready: boolean
-    html: string
-  }> => {
-    return got
-      .get(
-        'https://api.products.aspose.app/barcode/id/recognize/recognizeresult/' +
-          recognizeResultToken,
-        {
-          searchParams: {
-            timestamp: new Date().getTime()
-          }
-        }
-      )
-      .json()
-  }
-  let next = true
-  setTimeout(() => {
-    next = false
-  }, 20_000)
-  let html = ''
-  while (next) {
-    let ready = await getReady(responseRecognize.recognizeResultToken)
-    if (ready.ready) {
-      html = ready.html
-      break
-    }
-  }
-  const $ = Cheerio.load(html)
-
-  return $('textarea').val()
-}
-
-const scanWithJsQr = (data: Buffer) => {
-  const imageData = getSync(data)
-  const code = jsQR(imageData.data, imageData.width, imageData.height)
-  if (!code) {
-    return null
-  }
-  return code.data
-}
+const QrCodeInvalid = 'QRCode tidak dikenali'
 const scanWithImgonline = async (data: Buffer) => {
   const form = new FormData()
 
-  form.append('uploadfile', data, 'qrcode')
+  form.append('uploadfile', data, 'qrcode' + Date.now())
   form.append('codetype', 2)
   form.append('rotset', 0)
   form.append('croptype', 1)
@@ -109,16 +26,48 @@ const scanWithImgonline = async (data: Buffer) => {
     })
     .text()
   const $ = Cheerio.load(response)
-  return $('#content div').text().trim()
+  const result = $('#content div').text().trim()
+  if (!result) throw new Error(QrCodeInvalid)
+  return result
 }
+
+const scanWithJimpQrcode = (data: Buffer): Promise<string | null> =>
+  new Promise((resolve, reject) => {
+    Jimp.read(data, function (err, image) {
+      if (err) {
+        reject(new Error(QrCodeInvalid))
+      }
+      let qrcode = new QrCode()
+      qrcode.callback = function (err2: any, value: any) {
+        if (err2) {
+          reject(new Error(QrCodeInvalid))
+        }
+        if (value == undefined || value?.result == null || value?.result == '')
+          return reject(new Error(QrCodeInvalid))
+        resolve(value.result)
+      }
+      qrcode.decode(image.bitmap)
+    })
+  })
+
+const scanWithJsQr = (data: Buffer) =>
+  new Promise((resolve, reject) => {
+    const imagedata = getSync(data)
+    const code = jsQR(imagedata.data, imagedata.width, imagedata.height)
+    if (!code) return reject(new Error(QrCodeInvalid))
+
+    resolve(code.data)
+  })
 
 export default async function (data: Buffer): Promise<string> {
   let result: string | null = null
-  result = scanWithJsQr(data)
-  if (!result) result = await scanWithImgonline(data)
-  if (!result) result = await scanWithJimpQrcode(data)
-  if (!result) result = await scanWithApi(data)
-  if (!result) throw new Error('QR Code tidak dikenali')
+  // @ts-ignore
+  result = await Promise.any([
+    scanWithJsQr(data),
+    scanWithImgonline(data),
+    scanWithJimpQrcode(data)
+  ])
+  if (!result) throw new Error(QrCodeInvalid)
 
   return result
 }
